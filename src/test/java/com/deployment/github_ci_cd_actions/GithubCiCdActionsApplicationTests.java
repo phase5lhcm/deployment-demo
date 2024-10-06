7b319e7a307913bc17190bbec32d3e3e26ab4016
Christine <84812987+phase5lhcm@users.noreply.github.com>
2024-10-06 15:40:58 -0400

    First commit

---
+++ src/test/java/com/deployment/github_ci_cd_actions/GithubCiCdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment.github_ci_cd_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCiCdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
